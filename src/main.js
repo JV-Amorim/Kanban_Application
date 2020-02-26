86f2a829d37569035224cd51f1fe3289121a83ba
João Vitor Amorim <joaovitoramorimx@gmail.com>
2020-02-25 22:26:34 -0300

    Resources to front-end development added: Axios, Babel, Webpack. Create basic structure to front-end folders.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/api',
+});
+
+async function readLists() {
+    const response = await api.get('/lists');
+
+    console.log(response);
+}
+
+readLists();
